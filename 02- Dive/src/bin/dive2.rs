64a9a9960e1de984a44501dabb1e7a366e89406e
quentin <quentin@minster.io>
2021-12-02 08:58:41 +0000

    day 2

---
+++ 02- Dive/src/bin/dive2.rs
@@ -0,0 +1,37 @@
+use std::io;
+use std::error::Error;
+
+fn main() -> Result<(),Box<dyn Error>> {
+    let stdin = io::stdin();
+
+    let mut position: i32 = 0;
+    let mut depth: i32 = 0;
+    let mut aim: i32 = 0;
+
+    loop {
+        let mut buffer = String::new();
+        let read = stdin.read_line(&mut buffer)?;
+        if read == 0 {
+            break;
+        }
+
+        let mut tokens = buffer.split_whitespace();
+        let movement = tokens.next().unwrap();
+        let value: i32 = tokens.next().unwrap().parse()?;
+
+        match movement {
+            "forward" => {
+                position += value;
+                depth += aim * value;
+            },
+            "up" => aim -= value,
+            "down" => aim += value,
+            _ => eprintln!("unknown command: {}", movement),
+        }
+    }
+
+    println!("position: {}", position);
+    println!("depth: {}", depth);
+    println!("(aim: {})", aim);
+    Ok(())
+}
