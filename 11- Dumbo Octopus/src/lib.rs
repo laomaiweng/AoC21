2caba35b6954392e40d39d109dd2b042f6929878
quentin <quentin@minster.io>
2021-12-11 21:46:56 +0100

    day 11: add "norecurse" feature

--- 11- Dumbo Octopus/src/lib.rs
+++ 11- Dumbo Octopus/src/lib.rs
@@ -30,6 +30,41 @@ impl Octopuses {
         flashes
     }
 
+    #[cfg(feature = "norecurse")]
+    fn flash(&mut self, row: usize, col: usize) -> u32 {
+        let (n, m) = self.grid.size();
+
+        let mut flashes = 0u32;
+        let mut flashers: Vec<(usize, usize)> = vec![(row,col)];
+        while !flashers.is_empty() {
+            let (row,col) = flashers.pop().unwrap();
+            if self.grid[row][col] <= 9 {
+                continue;
+            }
+            self.grid[row][col] = 0;
+            flashes += 1;
+
+            let rmin = if row == 0 { 0 } else { row-1 };
+            let rmax = if row < n-1 { row+1 } else { row };
+            let cmin = if col == 0 { 0 } else { col-1 };
+            let cmax = if col < m-1 { col+1 } else { col };
+            for i in rmin..=rmax {
+                for j in cmin..=cmax {
+                    // only flash once
+                    if self.grid[i][j] != 0 {
+                        self.grid[i][j] += 1;
+                        if self.grid[i][j] > 9 {
+                            flashers.push((i,j));
+                        }
+                    }
+                }
+            }
+        }
+
+        flashes
+    }
+
+    #[cfg(not(feature = "norecurse"))]
     fn flash(&mut self, row: usize, col: usize) -> u32 {
         if self.grid[row][col] <= 9 {
             return 0;
